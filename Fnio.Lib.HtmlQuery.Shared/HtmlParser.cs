de1045698780560320ac7a3d7a6b4c65f1dab5ac
johnsmith1st <johnsmith1st@live.com>
2016-03-10 00:11:17 +0800

    [update] html parser

--- Fnio.Lib.HtmlQuery.Shared/HtmlParser.cs
+++ Fnio.Lib.HtmlQuery.Shared/HtmlParser.cs
@@ -8,22 +8,22 @@ using System.Xml.Linq;
 
 namespace Fnio.Lib.HtmlQuery
 {
-    static class HtmlParser
+    public static class HtmlParser
     {
         public static HtmlDocument Parse(string html)
         {
             return Parse(html, null);
         }
 
-        public static HtmlDocument Parse(string html, IEnumerable<string> withoutTags)
+        public static HtmlDocument Parse(string html, IEnumerable<string> filterTags)
         {
             html = TrimHtml(html);
 
             var xdoc = ParseXmlDocument(html);
             var htmlNode = GetHtmlNode(xdoc);
 
-            var tagSet = withoutTags != null
-                ? new HashSet<string>(withoutTags.Select(s => s.ToLowerInvariant()))
+            var tagSet = filterTags != null
+                ? new HashSet<string>(filterTags.Select(s => s.ToLowerInvariant()))
                 : null;
 
             if (htmlNode == null) return null;
@@ -72,10 +72,10 @@ namespace Fnio.Lib.HtmlQuery
                 );
         }
 
-        private static HtmlNode ToHtmlNode(this XNode x, HtmlDocument doc, HashSet<string> withoutTags)
+        private static HtmlNode ToHtmlNode(this XNode x, HtmlDocument doc, HashSet<string> filterTags)
         {
             if (x.NodeType == XmlNodeType.Element && x is XElement)
-                return ToHtmlElement(x as XElement, doc, withoutTags);
+                return ToHtmlElement(x as XElement, doc, filterTags);
 
             if (x.NodeType == XmlNodeType.Text && x is XText)
                 return ToHtmlTextNode(x as XText, doc);
@@ -83,15 +83,15 @@ namespace Fnio.Lib.HtmlQuery
             return null;
         }
 
-        private static HtmlElement ToHtmlElement(this XElement x, HtmlDocument doc, HashSet<string> withoutTags)
+        private static HtmlElement ToHtmlElement(this XElement x, HtmlDocument doc, HashSet<string> filterTags)
         {
             var tag = x.Name.LocalName.ToLowerInvariant();
 
-            if (withoutTags != null && withoutTags.Contains(tag))
+            if (filterTags != null && filterTags.Contains(tag))
                 return null;
 
             var attrs = x.GetHtmlAttributes();
-            var childNodes = x.Nodes().Select(n => n.ToHtmlNode(doc, withoutTags)).Where(n => n != null);
+            var childNodes = x.Nodes().Select(n => n.ToHtmlNode(doc, filterTags)).Where(n => n != null);
             return doc.CreateElement(tag, attrs, childNodes);
         }
 
