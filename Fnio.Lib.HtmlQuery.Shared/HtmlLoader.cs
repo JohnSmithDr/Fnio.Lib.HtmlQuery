3f4ce18646de1390cf83e9fe50bad6cd7c7686e8
johnsmith1st <johnsmith1st@live.com>
2016-03-10 00:21:56 +0800

    [add] html loader

---
+++ Fnio.Lib.HtmlQuery.Shared/HtmlLoader.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Fnio.Lib.HtmlQuery
+{
+    public static partial class HtmlLoader
+    {
+        private static readonly Regex MetaCharsetRegex =
+            new Regex("<meta(?!\\s*(?:name|value)\\s*=)[^>]*?charset\\s*=[\\s\"']*([^\\s\"'/>]*)");
+    }
+}
