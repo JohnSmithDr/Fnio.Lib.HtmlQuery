530638a4b3565e2fc5b561871d2bedd0671b5483
johnsmith1st <johnsmith1st@live.com>
2016-03-13 13:02:42 +0800

    [add] ancestors query, and pass tests

---
+++ Fnio.Lib.HtmlQuery.Shared/Query/Ancestors.cs
@@ -0,0 +1,47 @@
+﻿using Fnio.Lib.HtmlQuery.Node;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fnio.Lib.HtmlQuery
+{
+    public static partial class HtmlQuery
+    {
+        public static IEnumerable<HtmlElement> GetAncestors(this HtmlNode n)
+        {
+            return GetAncestorsEnumerable(n).Reverse();
+        }
+
+        public static IEnumerable<HtmlElement> GetAncestors(this HtmlNode n, Func<HtmlElement, bool> predicate)
+        {
+            return n.GetAncestors().Where(predicate);
+        }
+
+        public static HtmlElement GetAncestorById(this HtmlNode n, string id)
+        {
+            return n.GetAncestors().Where(s => s.Id == id).FirstOrDefault();
+        }
+
+        public static IEnumerable<HtmlElement> GetAncestorsByClassName(this HtmlNode n, string className)
+        {
+            return n.GetAncestors().Where(c => c.ClassNames.Contains(className));
+        }
+
+        public static IEnumerable<HtmlElement> GetAncestorsByTagName(this HtmlNode n, string tagName)
+        {
+            tagName = tagName.ToLowerInvariant();
+            return n.GetAncestors().Where(s => s.TagName == tagName);
+        }
+
+        private static IEnumerable<HtmlElement> GetAncestorsEnumerable(HtmlNode n)
+        {
+            var parent = n.Parent as HtmlElement;
+            while(parent != null)
+            {
+                yield return parent;
+                parent = parent.Parent as HtmlElement;
+            }
+        }
+    }
+}
---
+++ Fnio.Lib.HtmlQuery.UnitTest/Query/AncestorsTest.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Fnio.Lib.HtmlQuery.Node;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fnio.Lib.HtmlQuery.UnitTest
+{
+    public partial class HtmlQueryTest
+    {
+        [TestMethod]
+        public void TestGetAncestors()
+        {
+            Doc.GetElementById("logo").GetAncestors().Select(s => s.TagName).Should().Equal("html", "body", "header");
+            Doc.GetElementById("home-link").GetAncestors().Select(s => s.TagName).Should().Equal("html", "body", "nav");
+            Doc.GetElementsByClassName("info").FirstOrDefault().GetAncestors(s => s.TagName == "div").Should().HaveCount(1);
+            Doc.GetElementsByClassName("info").FirstOrDefault().GetAncestors(s => s.TagName == "p").Should().HaveCount(0);
+        }
+
+        [TestMethod]
+        public void TestGetAncestorById()
+        {
+            Doc.GetElementById("home-link").GetAncestorById("navigation").Should().NotBeNull();
+            Doc.GetElementById("logo").GetAncestorById("header").Should().BeNull();
+        }
+
+        [TestMethod]
+        public void TestGetAncestorsByClassName()
+        {
+            Doc.GetElementById("home-link").GetAncestorsByClassName("container").Should().HaveCount(1);
+            Doc.GetElementById("logo").GetAncestorsByClassName("container").Should().HaveCount(1);
+            Doc.GetElementById("container").GetAncestorsByClassName("wrapper").Should().HaveCount(0);
+        }
+
+        [TestMethod]
+        public void TestGetAncestorsByTagName()
+        {
+            Doc.GetElementById("home-link").GetAncestorsByTagName("nav").Should().HaveCount(1);
+            Doc.GetElementById("logo").GetAncestorsByTagName("body").Should().HaveCount(1);
+            Doc.GetElementById("container").GetAncestorsByTagName("div").Should().HaveCount(0);
+        }
+    }
+}
