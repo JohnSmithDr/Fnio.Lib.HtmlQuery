074a56492ff9dab3788a59f9787efe4961178251
johnsmith1st <johnsmith1st@live.com>
2016-03-17 23:54:21 +0800

    [add] HtmlLoader to portable lib

---
+++ Fnio.Lib.HtmlQuery.Portable/HtmlLoader.cs
@@ -0,0 +1,74 @@
+﻿using Fnio.Lib.HtmlQuery.Node;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Foundation;
+using Windows.Web.Http;
+
+namespace Fnio.Lib.HtmlQuery
+{
+    public static partial class HtmlLoader
+    {
+        public static IAsyncOperationWithProgress<string, HttpProgress> LoadHtmlAsync(HttpClient httpClient, Uri uri)
+        {
+            return AsyncInfo.Run<string, HttpProgress>(async (c, p) =>
+            {
+                var response = await httpClient.GetAsync(uri, HttpCompletionOption.ResponseContentRead).AsTask(c, p);
+                using (response)
+                {
+                    string charset = null;
+
+                    // try get charset from content-type
+                    //
+                    if (response.Content.Headers.ContentType != null)
+                    {
+                        charset = response.Content.Headers.ContentType.CharSet ?? null;
+                        Debug.WriteLine("Charset from http header content-type: " + charset);
+                    }
+
+                    // try get charset from meta tag
+                    //
+                    if (string.IsNullOrEmpty(charset))
+                    {
+                        var html = await response.Content.ReadAsStringAsync();
+                        var match = MetaCharsetRegex.Match(html);
+                        charset = match?.Groups[1].Value.Trim() ?? null;
+                        Debug.WriteLine("Charset from html meta tag: " + charset);
+                    }
+
+                    // or default charset is utf-8
+                    //
+                    if (string.IsNullOrEmpty(charset))
+                    {
+                        charset = "utf-8";
+                    }
+
+                    var encoding = Portable.Text.Encoding.GetEncoding(charset) ?? Encoding.UTF8;
+                    var buff = await response.Content.ReadAsBufferAsync();
+                    var data = buff.ToArray();
+                    var result = encoding.GetString(data, 0, data.Length);
+                    return result;
+                }
+            });
+        }
+
+        public static IAsyncOperationWithProgress<HtmlDocument, HttpProgress> LoadHtmlDocumentAsync(HttpClient httpClient, Uri uri)
+        {
+            return LoadHtmlDocumentAsync(httpClient, uri, null);
+        }
+
+        public static IAsyncOperationWithProgress<HtmlDocument, HttpProgress> LoadHtmlDocumentAsync(HttpClient httpClient, Uri uri, IEnumerable<string> tagFilters)
+        {
+            return AsyncInfo.Run<HtmlDocument, HttpProgress>(async (c, p) =>
+            {
+                var html = await LoadHtmlAsync(httpClient, uri).AsTask(c, p);
+                var htmlDoc = HtmlParser.Parse(html, tagFilters);
+                htmlDoc.Url = uri;
+                return htmlDoc;
+            });
+        }
+    }
+}
